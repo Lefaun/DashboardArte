92004c1f26e8003f26b387b29455ae9713f0a651
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-10 18:19:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 import pandas as pd
 df = pd.read_csv('arte-urbana - arte_urbana_fev2022.csv')
-#df = pd.read_csv('https://query.data.world/s/edvqfu4ea2ap6sbmr7ht2szxjc2mij')
+df = pd.read_csv('https://query.data.world/s/edvqfu4ea2ap6sbmr7ht2szxjc2mij')
 placeholder = st.empty()
 #Title
 st.title = "Real time computer Science Dashboard 1"
